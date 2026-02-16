137159b6892718a05b341e5229d2e7b85d0d6e24
aknysh <andriy.knysh@gmail.com>
2026-02-16 14:52:47 -0500

    update component

--- src/providers.tf
+++ src/providers.tf
@@ -11,6 +11,18 @@ provider "aws" {
   }
 }
 
+provider "awsutils" {
+  region = var.region
+
+  profile = var.account_map_enabled && !var.privileged && module.iam_roles.profiles_enabled ? coalesce(var.import_profile_name, module.iam_roles.terraform_profile_name) : null
+  dynamic "assume_role" {
+    for_each = !var.account_map_enabled || var.privileged || module.iam_roles.profiles_enabled ? [] : ["role"]
+    content {
+      role_arn = coalesce(var.import_role_arn, module.iam_roles.terraform_role_arn)
+    }
+  }
+}
+
 module "iam_roles" {
   source     = "../account-map/modules/iam-roles"
   privileged = var.privileged
